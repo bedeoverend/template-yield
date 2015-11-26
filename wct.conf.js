3a88c6e44b9e413ab96b2dd1876b92e3dd10d363
Bede <bede.overend@gmail.com>
2015-11-26 13:49:53 +1100

    setup remote testing using sauce labs

--- wct.conf.js
+++ wct.conf.js
@@ -1,7 +1,12 @@
 module.exports = {
   plugins: {
-    local: {
-      browsers: ['chrome']
+    sauce: {
+      disabled: true,
+      browsers: [
+        'OS X 10.10/chrome',
+        'OS X 10.10/firefox',
+        'OS X 10.10/safari'
+      ]
     }
   }
 };
